9072b800ab010c87b10015ae6ab15cadb1ec505c
Jiujiujoe <50279592+Jiujiujoe@users.noreply.github.com>
2020-05-07 14:29:42 -0700

    update leetcode2222

---
+++ src/main/java/com/techbow/worksimulation/sensoriot/ingest/leetcode2222/leetcode222.java
@@ -0,0 +1,10 @@
+package com.techbow.worksimulation.sensoriot.ingest.leetcode2222;
+
+public class leetcode222 {
+
+    private String handsome;
+
+    private void callme(){
+        System.out.println("I am handsonegit");
+    }
+}
